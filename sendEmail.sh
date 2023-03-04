f84b85d4ff3b786922673abf8fc05f6507fe641a
Gokul <gokuladitya2@gmail.com>
2023-03-04 09:46:51 +0530

    first release of sendEmail

---
+++ sendEmail.sh
@@ -0,0 +1,34 @@
+#!usr/bin/env bash
+
+# User input
+#read -p "Enter your email : " sender
+#to run in task scheduler
+#>cmd /c ""C:\Program Files\Git\bin\bash.exe" --login -i -- C:\Users\ththc\Desktop\sideproject\sendEmail.sh"
+#"C:\Program Files\Git\bin\bash.exe"
+#--login -i -- C:\Users\ththc\Desktop\sideproject\sendEmail.sh
+#max admin previlege
+
+gapp="gokuladitya13@gmail.com:ubxdpxsvssgspupm";
+sender="gokuladitya13@gmail.com";
+recipients="gokuladitya2@gmail.com,suksesh164@gmail.com";
+#split comma sepearated emailid in array
+IFS=', ' read -r -a array <<< "$recipients";
+sub="test sub";
+body="this is a test messsage";
+
+(echo -e "From: ${sender} \nTo: ${recipients} \nSubject:${sub} \n\n${body}")>emailTemplate.txt
+
+
+
+
+#store curl cmd for multiple address
+cmd="curl --ssl-reqd --url "smtps://smtp.gmail.com:465" --user "${gapp}" --mail-from \"${sender}\" --upload-file emailTemplate.txt"
+
+for element in "${array[@]}"
+do
+    cmd+=" --mail-rcpt \"${element}\""
+done
+
+echo $cmd
+#execute curl cmd
+$cmd
